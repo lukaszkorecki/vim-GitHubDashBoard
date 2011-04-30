9def2ce78d2b6a4c7cf9d1960a047f37d3739a29
Łukasz Korecki <lukasz@coffeesounds.com>
2011-04-30 22:07:08 +0100

    more events, gitconfig data

--- feed.rb
+++ feed.rb
@@ -2,14 +2,25 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'yaml'
 
 
 class Feed
   attr_reader :content
   def initialize user = '', token = ''
-    raise if user.empty? or token.empty?
+    if user.empty? or token.empty?
+      user, token = read_git_config
+    end
+    raise "Missing Github credentials" if user.empty? or token.empty?
     @url = URI.parse "https://github.com/#{user}.private.json?token=#{token}"
     self
+  end
+
+  def read_git_config
+    [
+      `git config -f ~/.gitconfig --get github.user`,
+      `git config -f ~/.gitconfig --get github.token`
+    ].map { |e| e.chomp.strip }
   end
 
   def download
@@ -39,16 +50,18 @@ class Feed
 
   def event_to_string event
     ot = "#{event['url'] || 'X'}|#{event['created_at']}|#{event['actor']} "
-    ot = "#{event['created_at']}|#{event['actor']} "
+    ot = "#{event['type']}|#{event['created_at']}|#{event['actor']} "
     ot << case event['type']
           when 'PushEvent'
-            "pushed to #{event['payload']['repo']}"
+            "pushed #{event['payload']['size']} commit#{event['payload']['size'] > 1 ? 's' : ''} to #{event['payload']['repo']}"
           when 'PullRequestEvent'
             "#{event['payload']['action']} pull request in #{event['payload']['repo']}"
           when 'IssuesEvent'
-            "#{event['payload']['action']} issue in #{event['payload']['repo']}"
-          when 'CommitCommentEvent'
+            "#{event['payload']['action']} issue ##{event['payload']['number']} in #{event['payload']['repo']}"
+          when 'IssueCommentEvent'
             "commented in #{event['payload']['repo']}"
+          when 'CommitCommentEvent'
+            "commented on commit #{event['payload']['commit'][0..6]} in #{event['payload']['repo']}"
           when 'WatchEvent'
             "#{event['payload']['action']} watching #{event['payload']['repo']}"
           when 'FollowEvent'
